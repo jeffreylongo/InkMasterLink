380adb3bad575c59578ad5a2c26f3ac749d5fbc2
Jeff Longo <jeffreylongo@yahoo.com>
2025-05-05 19:54:52 -0400

    ok

--- netlify/functions/shops.js
+++ netlify/functions/shops.js
@@ -6,14 +6,21 @@ exports.handler = async () => {
     const filePath = path.resolve(__dirname, "tattoo_shops.json");
     const data = fs.readFileSync(filePath, "utf-8");
     const shops = JSON.parse(data);
+
     return {
       statusCode: 200,
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(shops),
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: err.message, stack: err.stack }),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        message: "Error reading tattoo_shops.json",
+        error: err.message,
+        stack: err.stack,
+      }),
     };
   }
 };
