280321d1dd632773092f0d83824d2ceca728077d
Jeff Longo <jeffreylongo@yahoo.com>
2025-05-05 19:43:23 -0400

    ok

---
+++ netlify/netlify/functions/shops.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async () => {
+  try {
+    const filePath = path.resolve(__dirname, "../../data/tattoo_shops.json");
+    const data = fs.readFileSync(filePath, "utf-8");
+    const shops = JSON.parse(data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(shops),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to load shops" }),
+    };
+  }
+};
