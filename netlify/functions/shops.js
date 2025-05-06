529b3d4664593afa10ab60ee496e180936758de1
Jeff Longo <jeffreylongo@yahoo.com>
2025-05-06 12:10:49 -0400

    Update shops.js

--- netlify/functions/shops.js
+++ netlify/functions/shops.js
@@ -1,32 +1,22 @@
-const fs = require("fs");
-const path = require("path");
+// netlify/functions/shops.js
+
+const shopsData = require("./tattoo_shops.json");
 
 exports.handler = async function () {
   try {
-    // __dirname is /var/task (the deployed function dir)
-    const filePath = path.resolve(__dirname, "data", "tattoo_shops.json");
-
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-    const json = JSON.parse(fileContent);
-
-    if (!Array.isArray(json)) {
-      throw new Error("JSON is not an array");
-    }
-
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(json)
+      body: JSON.stringify(shopsData)
     };
   } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: err.message,
-        where: "shops.js",
-        stack: err.stack
+        error: "Could not load shop data",
+        details: err.message
       })
     };
   }
